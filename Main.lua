e28c75f66c0bea344a3f10c198d08faae63d9a71
Gravvyyy <115034650+Gravvyyy@users.noreply.github.com>
2022-10-15 09:55:33 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,6 @@
-Coming Very Soon !!!!!!
+Saturn Hub is coming very soon 
+
+For updates join the discord server : https://discord.gg/35VAgesB6h
+
+Devs: Mazza : https://github.com/Mazzass 
+      Gravy : https://github.com/Gravvyyy 
